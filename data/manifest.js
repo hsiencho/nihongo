e63eb3a29af665af569b64777782da5cf0e34db6
hsiencho <hsiencho777@gmail.com>
2026-03-17 00:20:40 +0800

    Update manifest.js

--- data/manifest.js
+++ data/manifest.js
@@ -1,8 +1,9 @@
 window.courseList = [
     "N5_vocab.js",
-    "n5_vocab_99.js",
+    "N4_vocab.js",
     "anime_you_and_i_ep1.js",
     "anime_you_and_i_ep2.js",
     "song_megane.js",
     "song_hitoribotchi.js",
+    "n5_vocab_99.js",
 ];
